26ac80459ce0b1f3f3450724782008ccd3356ed5
Luke Jones <manhappylife@yahoo.ca>
2017-04-02 21:19:25 -0400

    Helpers
    The load/store isn't confirmed yet cause i can't get this to run

--- JOOSA-src/patterns.h
+++ JOOSA-src/patterns.h
@@ -10,6 +10,68 @@
  * email: hendren@cs.mcgill.ca, mis@brics.dk
  */
 
+
+/*
+ * HELPERS
+ */
+
+/*
+ * Checks if the given load (returns false if not load) is the last
+ * before another store of the same index. If there are any jump
+ * operations return false since we cannot conclude they wont jump
+ * somewhere that uses a load of the same index
+ */
+int is_last_value_load(CODE *c) {
+  int loadInd, storeInd, testLoadInd, labelBuf;
+  if (is_aload(c, &loadInd)) {
+    /* a load stuff */
+    CODE *cur = next(c);
+    /* while cur isn't null and isn't an astore with same value */
+    while(cur && !(is_astore(cur, &storeInd) && storeInd == loadInd)) {
+      if (is_aload(cur, &testLoadInd) && testLoadInd == loadInd)
+        return 0;
+      if (uses_label(cur, &labelBuf))
+        return 0;
+      cur = next(cur);
+    }
+    /* If we didn't find a corresponding load then this must be last load */
+    return 1;
+  }
+  else if (is_iload(c, &loadInd)) {
+    /* i load stuff */
+    CODE *cur = next(c);
+    /* while cur isn't null and isn't an astore with same value */
+    while(cur && !(is_istore(cur, &storeInd) && storeInd == loadInd)) {
+      if (is_iload(cur, &testLoadInd) && testLoadInd == loadInd)
+        return 0;
+      if (uses_label(cur, &labelBuf))
+        return 0;
+      cur = next(cur);
+    }
+    /* If we didn't find a corresponding load then this must be last load */
+    return 1;
+  }
+  else {
+    return 0;
+  }
+}
+
+/*
+ * Checks if this code is jumped to by any labels
+ */
+int is_label_destination(CODE *c) {
+  int i;
+  for (i = 0; i < currentlabelstablesize; i++) {
+    if (currentlabels[i].position == c)
+      return 1;
+  }
+  return 0;
+}
+
+/*
+ * PATTERNS
+ */
+
 /* iload x        iload x        iload x
  * ldc 0          ldc 1          ldc 2
  * imul           imul           imul
@@ -93,7 +155,6 @@ int simplify_goto_goto(CODE **c)
   return 0;
 }
 
-
 /* dup
  * istore_x
  * pop
@@ -144,6 +205,40 @@ int simplify_istore_0_double_branch(CODE **c) {
   return 0;
 }
 
+/* store_x
+ * load_x
+ * ...
+ * no more load_x before another store_x
+ * no labels jumping to load_x
+ * no more jump operations before another store_x
+ * (We could generalize this rule with more work to account for jumps
+ * that don't violate the soundness but for now that's too complex)
+ * --------->
+ * remove store/load combo
+ *
+ * sound because store/load removes then adds back to the stack. Since
+ * we no longer need to use that value and there's no jumps, there's nothing
+ * that relies on this store, and therefore we can just remove it.
+ */
+int remove_superfluous_storeloads(CODE **c)
+{
+  int storeInd, loadInd;
+  if (is_astore(*c, &storeInd) &&
+      is_aload(next(*c), &loadInd) &&
+      !is_label_destination(next(*c)) &&
+      storeInd == loadInd &&
+      is_last_value_load(next(*c))) {
+    return replace(c, 2, NULL);
+  }
+  else if (is_istore(*c, &storeInd) &&
+      is_iload(next(*c), &storeInd) &&
+      !is_label_destination(next(*c)) &&
+      storeInd == loadInd &&
+      is_last_value_load(next(*c))) {
+    return replace(c, 2, NULL);
+  }
+  return 0;
+}
 
 void init_patterns(void) {
   ADD_PATTERN(simplify_multiplication_right);
@@ -152,4 +247,6 @@ void init_patterns(void) {
   ADD_PATTERN(simplify_goto_goto);
   ADD_PATTERN(simplify_istore);
   ADD_PATTERN(simplify_istore_0_double_branch);
+  ADD_PATTERN(remove_superfluous_storeloads);
 }
+
